15dc9640e39b1120fb29eaea3004565794a96180
liguangyu <1727857539@qq.com>
2024-08-26 17:51:00 +0800

    init project

---
+++ src/main/java/com/dps/DocumentProcessServer/DocumentProcessServerApplication.java
@@ -0,0 +1,13 @@
+package com.dps.DocumentProcessServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentProcessServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentProcessServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dps/DocumentProcessServer/DocumentProcessServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dps.DocumentProcessServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentProcessServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
